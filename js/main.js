ac50babaa576d8e8d6a70acb8f4b0fe86d2daab1
kozl0009 <kyryl_kozlov@MSHFD03.minto.local>
2014-01-26 13:07:28 -0500

    Finished header menu and search slider

---
+++ js/main.js
@@ -0,0 +1,36 @@
+var searchOpen = false;
+
+$(document).ready(function(){ 
+    //add to the array the ids of each animal element 
+    searchTrigger = $("#search-signin .search-trigger"); 
+	searchBar = $("#search-bar");
+	//searchBar.hide();
+    $(searchTrigger).click(function() {
+        // Hide the trigger image:
+        $('#search-signin').hide();
+		$('#social-links').hide();
+        // At the same time slide the div open
+        $(searchBar).animate({width:250},1000, function(){
+            searchOpen = true;			
+        });
+		
+		$(".searchBar").focus(function() {
+			$(this).val('');
+			searchOpen = false;
+		});	
+		$(".searchBar").focusout(function() {
+			searchOpen = true;
+		});	
+		
+    });
+	
+	$(document).click(function() {
+		if(searchOpen==true){
+			$(searchBar).animate({width:0},1000, function(){
+				$('#search-signin').show();
+				$('#social-links').show();	
+				searchOpen = false;
+			});
+		}
+	});
+}); 

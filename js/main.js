da4a94bd8ad7b2af887d8e7bbbd3ecc63f6cef95
kozl0009 <kozl0009@algonquinlive.com>
2014-01-27 00:32:28 -0500

    added more articles, and social media, fixed story-meta

--- js/main.js
+++ js/main.js
@@ -33,4 +33,21 @@ $(document).ready(function(){
 			});
 		}
 	});
+	
+	$(function() {
+    $( document ).tooltip({
+		position: {
+			my: "center bottom-20",
+			at: "center top",
+			using: function( position, feedback ) {
+			$( this ).css( position );
+				$( "<div>" )
+					.addClass( "arrow" )
+					.addClass( feedback.vertical )
+					.addClass( feedback.horizontal )
+					.appendTo( this );
+				}
+			}
+		});
+	});
 }); 
